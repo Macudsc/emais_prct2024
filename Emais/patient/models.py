a240e9fd00a5e0bc5942f839d9fc877045cfc01e
macudsc <monopolistlife@gmail.com>
2024-07-18 19:54:54 +0300

    v0.0.5 +myinfoprofiles

--- Emais/patient/models.py
+++ Emais/patient/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+#Profile
+class PatientMyInfo(models.Model):
+  user=models.OneToOneField(User, on_delete=models.CASCADE)
+  image=models.ImageField(blank=True, upload_to='_myinfo_images')
+  contact_number=models.CharField(max_length=50, default="+123456789")
+
+  def __str__(self):
+      return self.user.username
